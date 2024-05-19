c4469e8d3447b03bdcc201172395bac30e51b58a
mysoyul <vega2k_@naver.com>
2024-05-19 21:48:13 +0900

    CorsConfigurationSource add

--- src/main/java/com/bookmarker/api/BookMarkerApiApplication.java
+++ src/main/java/com/bookmarker/api/BookMarkerApiApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class BookMarkerApiApplication {
@@ -16,19 +19,32 @@ public class BookMarkerApiApplication {
 		SpringApplication.run(BookMarkerApiApplication.class, args);
 	}
 
+//	@Bean
+//	@Order(1)
+//	public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+//		CorsConfiguration config = new CorsConfiguration();
+//		config.setAllowCredentials(false);
+//		config.addAllowedOrigin("*");
+//		config.addAllowedHeader("*");
+//		config.addAllowedMethod("*");
+//		config.setMaxAge(6000L);
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/**", config);
+//		FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+//		filterBean.setOrder(0);
+//		return filterBean;
+//	}
+
 	@Bean
-	@Order(1)
-	public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(false);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		config.setMaxAge(6000L);
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers","Access-Control-Allow-Origin","Access-Control-Request-Method", "Access-Control-Request-Headers","Origin","Cache-Control", "Content-Type", "Authorization"));
+		configuration.setAllowedMethods(Arrays.asList("DELETE", "GET", "POST", "PATCH", "PUT"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
-		filterBean.setOrder(0);
-		return filterBean;
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
+
 }
--- src/main/java/com/bookmarker/api/config/WebConfig.java
+++ src/main/java/com/bookmarker/api/config/WebConfig.java
@@ -1,22 +1,25 @@
 package com.bookmarker.api.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("test")
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedOriginPatterns("*")
-                //.allowedHeaders("Authorization", "Content-Type")
-                .allowedHeaders("*")
-                .maxAge(3600);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
+//                .allowedOriginPatterns("*")
+//                //.allowedHeaders("Authorization", "Content-Type")
+//                .allowedHeaders("*")
+//                .allowCredentials(false)
+//                .maxAge(3600);
+//    }
 
 
 
